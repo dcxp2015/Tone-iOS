7cc02c9192b6fff6e629aa9aeed1859a5e7209e2
Danny Flax <dannyflax@Dannys-MBP.fios-router.home>
2015-07-14 16:45:34 -0400

    Initial Commit

---
+++ Tone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tone
+//
+//  Created by Danny Flax on 7/14/15.
+//  Copyright (c) 2015 Danny Flax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tone
+//
+//  Created by Danny Flax on 7/14/15.
+//  Copyright (c) 2015 Danny Flax. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
