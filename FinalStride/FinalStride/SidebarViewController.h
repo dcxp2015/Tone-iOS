b5ac2495ae4ac43fe146b6635d24dc206a439c3d
rsbballguy <rsbballguy@gmail.com>
2015-07-18 03:57:51 -0400

    Menu view when button pushed or swipe left
    finally…

---
+++ FinalStride/FinalStride/SidebarViewController.h
@@ -0,0 +1,14 @@
+//
+//  SidebarViewController.h
+//  FinalStride
+//
+//  Created by Rahul Sundararaman on 7/18/15.
+//  Copyright (c) 2015 Rahul Sundararaman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface SidebarViewController : UITableViewController
+
+@end
--- pageTest/pageTest/ViewController.h
+++ pageTest/pageTest/ViewController.h
@@ -1,8 +1,8 @@
 //
 //  ViewController.h
-//  FinalStride
+//  pageTest
 //
-//  Created by Rahul Sundararaman on 7/17/15.
+//  Created by Rahul Sundararaman on 7/18/15.
 //  Copyright (c) 2015 Rahul Sundararaman. All rights reserved.
 //
 
